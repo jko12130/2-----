20ee05c960c7c5ca665737f6e0f5a841f6d485eb
jko12130 <56510302+jko12130@users.noreply.github.com>
2019-11-08 23:23:15 +0900

    Add files via upload

---
+++ Scraping homework.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.music_ranking
+
+url = 'https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20191107'
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get(url, headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+'''
+title = #body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis
+artist = #body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.artist.ellipsis
+'''
+
+list_selector = "#body-content > div.newest-list > div > table > tbody > tr > td.info"
+
+rank = 1
+for music in soup.select(list_selector):
+    '''
+    title_raw = music.select_one(".title")
+    title_split1 = title_raw.split(">")
+    title_split2 = title_split1.split("<")
+    print(title_split2)
+    '''
+    title = music.select_one(".title")
+    artist = music.select_one(".artist")
+    print(rank,title,artist)
+    rank +=1
+
+    data = {
+        'rank': rank,
+        'title': title.text,
+        'artist': artist.text,
+    }
+
+    db.music_ranking_1107.insert_one(data)
